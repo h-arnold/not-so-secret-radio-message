d0c71c0dbb17d99775a20d02cc4e325bf177646d
h-arnold <78823477+h-arnold@users.noreply.github.com>
2021-02-09 22:17:18 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("This is a secret!")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Great! I love secrets!")
+})
+input.onGesture(Gesture.Shake, function () {
+    if (radio_group == 100) {
+        radio_group = 101
+        radio.setGroup(radio_group)
+        basic.showIcon(IconNames.SmallDiamond)
+    } else {
+        radio_group = 100
+        radio.setGroup(radio_group)
+        basic.showIcon(IconNames.SmallSquare)
+    }
+})
+let radio_group = 0
+radio_group = 100
+radio.setGroup(radio_group)
+basic.showIcon(IconNames.SmallSquare)
